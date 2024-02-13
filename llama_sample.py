7e99f6e7b6f76b4f280b4c77d3d23a123604a3b6
Adeesh Kolluru <kolluru.adeesh@gmail.com>
2024-02-13 16:22:32 +0000

    updates tokenization for sampling

--- llama_sample.py
+++ llama_sample.py
@@ -61,8 +61,6 @@ def prepare_model_and_tokenizer(args):
         load_in_8bit=True,
         device_map="auto",
     )
-    
-    model = PeftModel.from_pretrained(model, args.model_path, device_map="auto")
 
     tokenizer = LlamaTokenizer.from_pretrained(
         model_string,
@@ -74,7 +72,8 @@ def prepare_model_and_tokenizer(args):
     model.eval()
 
     special_tokens_dict = dict()
-
+    if tokenizer.pad_token is None:
+        special_tokens_dict["pad_token"] = DEFAULT_PAD_TOKEN
     if tokenizer.eos_token is None:
         special_tokens_dict["eos_token"] = DEFAULT_EOS_TOKEN
     if tokenizer.bos_token is None:
@@ -82,11 +81,38 @@ def prepare_model_and_tokenizer(args):
     if tokenizer.unk_token is None:
         special_tokens_dict["unk_token"] = DEFAULT_UNK_TOKEN
 
-    tokenizer.add_special_tokens(special_tokens_dict)
-    tokenizer.pad_token = tokenizer.eos_token
+    smart_tokenizer_and_embedding_resize(
+        special_tokens_dict=special_tokens_dict,
+        llama_tokenizer=tokenizer,
+        model=model,
+    )
 
+    model = PeftModel.from_pretrained(model, args.model_path, device_map="auto")
+    
     return model, tokenizer
 
+def smart_tokenizer_and_embedding_resize(
+    special_tokens_dict, 
+    llama_tokenizer, 
+    model,
+):
+    """Resize tokenizer and embedding.
+
+    Note: This is the unoptimized version that may make your embedding size not be divisible by 64.
+    """
+    num_new_tokens = llama_tokenizer.add_special_tokens(special_tokens_dict)
+    model.resize_token_embeddings(len(llama_tokenizer))
+
+    if num_new_tokens > 0:
+        input_embeddings = model.get_input_embeddings().weight.data
+        output_embeddings = model.get_output_embeddings().weight.data
+
+        input_embeddings_avg = input_embeddings[:-num_new_tokens].mean(dim=0, keepdim=True)
+        output_embeddings_avg = output_embeddings[:-num_new_tokens].mean(dim=0, keepdim=True)
+
+        input_embeddings[-num_new_tokens:] = input_embeddings_avg
+        output_embeddings[-num_new_tokens:] = output_embeddings_avg
+        
 def unconditional_sample(args):
     model, tokenizer = prepare_model_and_tokenizer(args)
 
